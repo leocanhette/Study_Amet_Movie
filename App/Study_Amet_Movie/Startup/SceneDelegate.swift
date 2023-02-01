480759d0c745965b0910c583abfa0c12103b4c0e
Leo Canhette <leonardo.canhette@gmail.com>
2023-02-01 12:57:23 -0300

    Add initial structure project

---
+++ App/Study_Amet_Movie/Startup/SceneDelegate.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+@main
+class SceneDelegate: UIResponder, UIWindowSceneDelegate, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }        
+    }
+}
